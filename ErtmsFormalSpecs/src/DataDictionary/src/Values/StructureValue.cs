ea620d8315d588ac0518b0b0cd62543ed0ef63af
LaurentFerier <laurent@ertmssolutions.com>
2013-05-06 18:34:30 +0200

    Big fix #92 : Rightsiding the StructureValue took too much time. The fix consists of executing the rightside operation when needed (and keep the result for further use)

--- ErtmsFormalSpecs/src/DataDictionary/src/Values/StructureValue.cs
+++ ErtmsFormalSpecs/src/DataDictionary/src/Values/StructureValue.cs
@@ -348,15 +348,6 @@ namespace DataDictionary.Values
             }
             retVal.Enclosing = variable;
 
-            foreach (KeyValuePair<string, Utils.INamable> pair in retVal.Val)
-            {
-                Variables.Variable var = pair.Value as Variables.Variable;
-                if (var != null)
-                {
-                    var.Value = var.Value.RightSide(var, false);
-                }
-            }
-
             return retVal;
         }
     }
