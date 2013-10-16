7ded3b87319aa6b50d7e681ade3a44bfaa496703
LaurentFerier <laurent@ertmssolutions.com>
2013-10-16 14:36:55 +0200

    Correctly select the commit in the VersionSelector Sort columns according to importance

--- ErtmsFormalSpecs/src/GUI/src/VersionSelector/VersionSelector.Designer.cs
+++ ErtmsFormalSpecs/src/GUI/src/VersionSelector/VersionSelector.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.dataGridView = new System.Windows.Forms.DataGridView();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.label1 = new System.Windows.Forms.Label();
             this.filterTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -73,15 +73,6 @@
             this.splitContainer1.SplitterDistance = 40;
             this.splitContainer1.TabIndex = 1;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Filter";
-            // 
             // filterTextBox
             // 
             this.filterTextBox.Location = new System.Drawing.Point(49, 13);
@@ -90,6 +81,15 @@
             this.filterTextBox.TabIndex = 1;
             this.filterTextBox.Text = "EA_*";
             this.filterTextBox.TextChanged += new System.EventHandler(this.filterTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter";
             // 
             // VersionSelector
             // 
--- ErtmsFormalSpecs/src/GUI/src/VersionSelector/VersionSelector.cs
+++ ErtmsFormalSpecs/src/GUI/src/VersionSelector/VersionSelector.cs
@@ -45,14 +45,44 @@ namespace GUI.VersionSelector
             dataGridView.DoubleClick += new EventHandler(dataGridView_DoubleClick);
         }
 
+        /// <summary>
+        /// When double clicking on an element of the version selector, 
+        /// select the corresponding Commit and close the window to continue work
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void dataGridView_DoubleClick(object sender, EventArgs e)
         {
-            Selected = dataGridView.SelectedCells[0].Value as Commit;
-            Close();
+            Selected = null;
+
+            if (dataGridView.SelectedCells.Count == 1)
+            {
+                Selected = ((List<DisplayObject>)dataGridView.DataSource)[dataGridView.SelectedCells[0].OwningRow.Index].Commit;
+            }
+
+            if (Selected != null)
+            {
+                Close();
+            }
         }
 
         private class DisplayObject
         {
+            /// <summary>
+            /// The date of the commit
+            /// </summary>
+            public DateTimeOffset Date { get { return Commit.Committer.When; } }
+
+            /// <summary>
+            /// The author of the commit
+            /// </summary>
+            public String Author { get { return Commit.Committer.Name; } }
+
+            /// <summary>
+            /// The message of the commit
+            /// </summary>
+            public String Message { get { return Commit.Message; } }
+
             /// <summary>
             /// The commit to be displayed
             /// </summary>
@@ -66,10 +96,6 @@ namespace GUI.VersionSelector
             {
                 Commit = commit;
             }
-
-            public String Author { get { return Commit.Committer.Name; } }
-            public String Message { get { return Commit.Message; } }
-            public DateTimeOffset Date { get { return Commit.Committer.When; } }
         }
 
         /// <summary>
