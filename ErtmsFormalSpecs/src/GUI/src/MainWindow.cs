f24119a6e7ba836ef0d2d45f309e4f5ef358ebe0
LaurentFerier <laurent@ertmssolutions.com>
2013-10-17 14:39:59 +0200

    Version 9.8.4

--- ErtmsFormalSpecs/src/GUI/src/MainWindow.cs
+++ ErtmsFormalSpecs/src/GUI/src/MainWindow.cs
@@ -192,7 +192,7 @@ namespace GUI
         /// <summary>
         /// The application version number
         /// </summary>
-        private string versionNumber = "0.9.8.3";
+        private string versionNumber = "0.9.8.4";
 
         /// <summary>
         /// The thread used to synchronize node names with their model
