5fd9fa2bc0e084f10867d840d7797d0a914cefec
LaurentFerier <laurent@ertmssolutions.com>
2013-10-29 14:20:19 +0100

    Set the version number in the assembly info

--- ErtmsFormalSpecs/src/GUI/Properties/AssemblyInfo.cs
+++ ErtmsFormalSpecs/src/GUI/Properties/AssemblyInfo.cs
@@ -20,11 +20,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ERTMSFormalSpecs")]
+[assembly: AssemblyDescription("The GUI for modeling /testing with EFS files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GUI")]
+[assembly: AssemblyCompany("ERTMS Solutions")]
+[assembly: AssemblyProduct("ERTMSFormalSpecs GUI")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -47,5 +47,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.9.*")]
+[assembly: AssemblyFileVersion("0.9.9")]
