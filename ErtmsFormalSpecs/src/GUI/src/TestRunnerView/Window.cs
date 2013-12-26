3fb9ac7d246cf22fd73f800805a64479b4292129
Svitlana-Lukicheva <svitlana@ertmssolutions.com>
2013-12-26 16:05:27 +0100

    Removed useless method

--- ErtmsFormalSpecs/src/GUI/src/TestRunnerView/Window.cs
+++ ErtmsFormalSpecs/src/GUI/src/TestRunnerView/Window.cs
@@ -443,23 +443,5 @@ namespace GUI.TestRunnerView
 
             Refresh();
         }
-
-        /// <summary>
-        /// Provides the model element currently selected in this IBaseForm
-        /// </summary>
-        public Utils.IModelElement Selected
-        {
-            get
-            {
-                Utils.IModelElement retVal = null;
-
-                if (TreeView != null && TreeView.Selected != null)
-                {
-                    retVal = TreeView.Selected.Model;
-                }
-
-                return retVal;
-            }
-        }
     }
 }
