ac0c63dd32a17b1bd7f472c68d295f53c73f5199
LaurentFerier <laurent@ertmssolutions.com>
2014-04-09 09:11:40 +0200

    Sample driver for the EFS IPC Interface

---
+++ ErtmsFormalSpecs/src/EFSDriver/EFSDriver.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EFSDriver
+{
+    partial class EFSDriver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cycleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cycleButton
+            // 
+            this.cycleButton.Location = new System.Drawing.Point(196, 181);
+            this.cycleButton.Name = "cycleButton";
+            this.cycleButton.Size = new System.Drawing.Size(75, 23);
+            this.cycleButton.TabIndex = 0;
+            this.cycleButton.Text = "Cycle";
+            this.cycleButton.UseVisualStyleBackColor = true;
+            this.cycleButton.Click += new System.EventHandler(this.cycleButton_Click);
+            // 
+            // EFSDriver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 354);
+            this.Controls.Add(this.cycleButton);
+            this.Name = "EFSDriver";
+            this.Text = "EFSDriver";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cycleButton;
+    }
+}
+
---
+++ ErtmsFormalSpecs/src/EFSDriver/EFSDriver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EFSDriver.EFSService;
+
+namespace EFSDriver
+{
+    public partial class EFSDriver : Form
+    {
+        /// <summary>
+        /// Access to EFS Service
+        /// </summary>
+        private EFSServiceClient EFS { get; set; }
+
+        public EFSDriver()
+        {
+            InitializeComponent();
+            EFS = new EFSServiceClient();
+        }
+
+        private void cycleButton_Click(object sender, EventArgs e)
+        {
+            EFS.Cycle(Priority.Verification);
+            EFS.Cycle(Priority.UpdateInternal);
+            EFS.Cycle(Priority.Process);
+            EFS.Cycle(Priority.UpdateOutput);
+            EFS.Cycle(Priority.CleanUp);
+        }
+    }
+}
