9b37a831d7e40cd2c1134f14df56ba4b82958bc6
James Oakey <james@ertmssolutions.com>
2014-06-18 18:52:07 +0200

    Added predefined function: Discontinuities.

---
+++ ErtmsFormalSpecs/src/DataDictionary/src/Functions/PredefinedFunctions/Discontinuities.cs
@@ -0,0 +1,143 @@
+﻿// ------------------------------------------------------------------------------
+// -- Copyright ERTMS Solutions
+// -- Licensed under the EUPL V.1.1
+// -- http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+// --
+// -- This file is part of ERTMSFormalSpec software and documentation
+// --
+// --  ERTMSFormalSpec is free software: you can redistribute it and/or modify
+// --  it under the terms of the EUPL General Public License, v.1.1
+// --
+// -- ERTMSFormalSpec is distributed in the hope that it will be useful,
+// -- but WITHOUT ANY WARRANTY; without even the implied warranty of
+// -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// --
+// ------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+
+namespace DataDictionary.Functions.PredefinedFunctions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Discontinuities : PredefinedFunction
+    {
+        /// <summary>
+        /// The MRSP function
+        /// </summary>
+        public Parameter Targets { get; private set; }
+
+
+        public override Types.Type ReturnType
+        {
+            get { return EFSSystem.findType(OverallNameSpaceFinder.INSTANCE.findByName(EFSSystem.Dictionaries[0], "Kernel.SpeedAndDistanceMonitoring.TargetSupervision"), "Kernel.SpeedAndDistanceMonitoring.TargetSupervision.Targets"); }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="efsSystem"></param>
+        public Discontinuities(EFSSystem efsSystem)
+            : base(efsSystem,"DISCONT")
+        {
+            Targets = (Parameter)Generated.acceptor.getFactory().createParameter();
+            Targets.Name = "Targets";
+            Targets.Type = EFSSystem.AnyType;
+            Targets.setFather(this);
+            FormalParameters.Add(Targets);
+        }
+
+        /// <summary>
+        /// Perform additional checks based on the parameter type
+        /// </summary>
+        /// <param name="root">The element on which the errors should be reported</param>
+        /// <param name="context">The evaluation context</param>
+        /// <param name="actualParameters">The parameters applied to this function call</param>
+        public override void additionalChecks(ModelElement root, Interpreter.InterpretationContext context, Dictionary<string, Interpreter.Expression> actualParameters)
+        {
+            CheckFunctionalParameter(root, context, actualParameters[Targets.Name], 1);
+        }
+
+        /// <summary>
+        /// Provides the value of the function
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="actuals"></param>
+        /// <returns></returns>
+        public override Values.IValue Evaluate(Interpreter.InterpretationContext context, Dictionary<Variables.Actual, Values.IValue> actuals)
+        {
+            Values.IValue retVal = null;
+
+            int token = context.LocalScope.PushContext();
+            AssignParameters(context, actuals);
+
+            Types.Collection collectionType = (Types.Collection)EFSSystem.findType(OverallNameSpaceFinder.INSTANCE.findByName(EFSSystem.Dictionaries[0], "Kernel.SpeedAndDistanceMonitoring.TargetSupervision"), "Kernel.SpeedAndDistanceMonitoring.TargetSupervision.Targets");
+            Values.ListValue collection = new Values.ListValue(collectionType, new List<Values.IValue>());
+
+            Function function = context.findOnStack(Targets).Value as Functions.Function;
+            if (function != null && !function.Name.Equals("EMPTY"))
+            {
+                Graph graph1 = createGraphForValue(context, function);
+                ComputeTargets(graph1.Function, collection);
+            }
+
+            context.LocalScope.PopContext(token);
+
+            retVal = collection;
+            return retVal;
+        }
+
+        /// <summary>
+        /// Computes targets from the function and stores them in the collection
+        /// </summary>
+        /// <param name="function">Function containing targets</param>
+        /// <param name="collection">Collection to be filled with targets</param>
+        private void ComputeTargets(Function function, Values.ListValue collection)
+        {
+            bool retVal = true;
+            if (function != null)
+            {
+                Graph graph = function.Graph;
+                if (graph != null && graph.Segments.Count > 1)
+                {
+                    for (int i = 1; i < graph.Segments.Count; i++)
+                    {
+                        Graph.Segment s = graph.Segments[i];
+                        Types.Structure structureType = (Types.Structure)EFSSystem.findType(OverallNameSpaceFinder.INSTANCE.findByName(EFSSystem.Dictionaries[0], "Kernel.SpeedAndDistanceMonitoring.TargetSupervision"), "Kernel.SpeedAndDistanceMonitoring.TargetSupervision.Target");
+                        Values.StructureValue value = new Values.StructureValue(structureType);
+
+                        Variables.Variable speed = (Variables.Variable)DataDictionary.Generated.acceptor.getFactory().createVariable();
+                        speed.Type = EFSSystem.findType(OverallNameSpaceFinder.INSTANCE.findByName(EFSSystem.Dictionaries[0], "Default.BaseTypes"), "Default.BaseTypes.Speed");
+                        speed.Name = "Speed";
+                        speed.Mode = Generated.acceptor.VariableModeEnumType.aInternal;
+                        speed.Default = "0.0";
+                        speed.Enclosing = value;
+                        speed.Value = new Values.DoubleValue(EFSSystem.DoubleType, s.Val(s.Start));
+                        value.set(speed);
+
+                        Variables.Variable location = (Variables.Variable)DataDictionary.Generated.acceptor.getFactory().createVariable();
+                        location.Type = EFSSystem.findType(OverallNameSpaceFinder.INSTANCE.findByName(EFSSystem.Dictionaries[0], "Default.BaseTypes"), "Default.BaseTypes.Distance");
+                        location.Name = "Location";
+                        location.Mode = Generated.acceptor.VariableModeEnumType.aInternal;
+                        location.Default = "0.0";
+                        location.Enclosing = value;
+                        location.Value = new Values.DoubleValue(EFSSystem.DoubleType, s.Start);
+                        value.set(location);
+
+                        Variables.Variable length = (Variables.Variable)DataDictionary.Generated.acceptor.getFactory().createVariable();
+                        length.Type = EFSSystem.findType(OverallNameSpaceFinder.INSTANCE.findByName(EFSSystem.Dictionaries[0], "Default.BaseTypes"), "Default.BaseTypes.Length");
+                        length.Name = "Length";
+                        length.Mode = Generated.acceptor.VariableModeEnumType.aInternal;
+                        length.Default = "0.0";
+                        length.Enclosing = value;
+                        length.Value = new Values.DoubleValue(EFSSystem.DoubleType, s.End);
+                        value.set(length);
+
+                        collection.Val.Add(value);
+                    }
+                }
+            }
+        }
+    }
+}
