1b03f69be9ee63fa0a131549c903eec57c0c9b76
laurent <laurent@ertmssolutions.com>
2014-08-04 18:28:40 +0200

    Allow to perform a textual entry

---
+++ ErtmsFormalSpecs/src/GUI/src/TextEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class TextEntry : Form
+    {
+        public TextEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public String Value { get { return valueTextBox.Text; } }
+    }
+}
