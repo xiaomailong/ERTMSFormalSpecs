0c9591843f92206fa8e0daa9514da646044b6f94
JamesOakey <james@ertmssolutions.com>
2015-04-13 11:38:41 +0200

    Added test to check that an ambiguous EFS expression (that could designate to a variable or a type) will be interpreted as the variable.

---
+++ ErtmsFormalSpecs/src/DataDictionary.test/SemanticAnalysisTest.cs
@@ -0,0 +1,38 @@
+﻿using DataDictionary.Interpreter;
+using DataDictionary.Types;
+using DataDictionary.Values;
+using NUnit.Framework;
+using NameSpace = DataDictionary.Types.NameSpace;
+using Structure = DataDictionary.Types.Structure;
+using Variable = DataDictionary.Variables.Variable;
+using StructureElement = DataDictionary.Types.StructureElement;
+
+
+namespace DataDictionary.test
+{
+    [TestFixture]
+    class SemanticAnalysisTest : BaseModelTest
+    {
+        /// <summary>
+        /// Tests that an EFS expression that can refer to either a variable or a type will reference the variable
+        /// </summary>
+        [Test]
+        public void TestVariableAndTypeWithSameName()
+        {
+            Dictionary dictionary = CreateDictionary("Test");
+            NameSpace nameSpace = CreateNameSpace(dictionary, "NameSpace");
+
+            Structure structure = CreateStructure(nameSpace, "ModelElement");
+            StructureElement structElem = CreateStructureElement(structure, "Value", "Boolean");
+            structElem.setDefault("True"); 
+
+            Variable variable = CreateVariable(nameSpace, "ModelElement", "ModelElement");
+            variable.SubVariables["Value"].Value = new BoolValue(structElem.Type, false);
+
+            Expression expression = Parser.Expression(dictionary, "NameSpace.ModelElement");
+            IValue value = expression.GetValue(new InterpretationContext(), null);
+
+            Assert.AreEqual(value, variable.Value);
+        }
+    }
+}
